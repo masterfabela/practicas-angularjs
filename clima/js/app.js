d3b42946ae20531a44ba5a2ba2c9af53acaa2006
masterfabela <fabelaretorcida@gmail.com>
2018-11-26 17:42:20 +0100

    Filtro de fecha

--- clima/js/app.js
+++ clima/js/app.js
@@ -1,28 +1,25 @@
-(function(){
+(function() {
+  var app = angular.module('ejemplosApp', []);
 
-var app = angular.module('ejemplosApp',[ ]);
-
-
-
-app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
-  
-  //http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&callback=JSON_CALLBACK
-	$scope.clima = {};
-
-	$http.jsonp('http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&callback=JSON_CALLBACK&appid=9f50a805aa0089a1edd1829a5db029f0')
-		.success(function(data){
-
-			$scope.clima = data;
-
-
-		});
-
-
-
-}]);
-
-
-
-
-
+  app.controller('mainCtrl', [
+    '$scope',
+    '$http',
+    function($scope, $http) {
+      //http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&callback=JSON_CALLBACK
+      $scope.clima = {};
+      $scope.kelvinCelsius = function(kelvins) {
+        var celsius;
+        celsius = kelvins - 273.15;
+        celsius = Math.round(celsius * 100) / 100;
+        return celsius;
+      };
+      $http
+        .jsonp(
+          'http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&callback=JSON_CALLBACK&appid=9f50a805aa0089a1edd1829a5db029f0'
+        )
+        .then(function(data) {
+          $scope.clima = data.data;
+        });
+    }
+  ]);
 })();
