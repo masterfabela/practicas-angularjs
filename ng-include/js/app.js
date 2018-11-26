8816cf2f683a97549707bc51136998116eb43b08
masterfabela <fabelaretorcida@gmail.com>
2018-11-26 17:17:02 +0100

    ng-inclde

---
+++ ng-include/js/app.js
@@ -0,0 +1,17 @@
+(function() {
+  var app = angular.module('ejemplosApp', []);
+
+  app.controller('mainCtrl', [
+    '$scope',
+    '$http',
+    function($scope, $http) {
+      $scope.profesores = {};
+      $http.get('json/profesores.json').success(function(data) {
+        //Codigo cuando es correcta la petición
+        $scope.profesores = data.profesores;
+      });
+
+      $scope.tablaProfesores = 'parciales/tablaProfesores.html';
+    }
+  ]);
+})();
